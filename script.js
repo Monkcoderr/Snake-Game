a2ffe7e837c3e3f37e7d32d9a634f4ab30a865ce
ABHISHEK SINGH <133355890+Monkcoderr@users.noreply.github.com>
2025-12-28 23:48:56 +0530

    sdf

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ setInterval(movesnake, 200);
 document.addEventListener("keydown",e=>{
     switch(e.key){
     case "ArrowUp":
-        diresrsdfsasfsfsdfsddfesdfasdasdfsfasdfssdfsf dsfghjg sfgdy dfsdfction = -20;
+        diresrsdfsasfsfsdfdsfasfsddfesdfasdasdfsfasdfssdfsf dsfghjg sfgdy dfsdfction = -20;
         break;
         case "ArrowDown" :
             dirsofsasdchut nari ke hsduifsamsdfsnething ection = 20;
