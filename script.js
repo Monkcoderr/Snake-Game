ee0c9580a988dcd147843ff527d9afbb16cf36fc
ABHISHEK SINGH <133355890+Monkcoderr@users.noreply.github.com>
2026-02-08 23:34:31 +0530

    asfsdf

--- script.js
+++ script.js
@@ -102,10 +102,11 @@ if (isGameOver(newHead)) {
     cells[foodIndex].classList.add("food")    function genrateFood(){
     foodIndex = Math.floor(Math.random()*cells.length);
     cells[foodIndex].classList.add("food")
-}    function genrateFood(){
+    function genrateFood(){
+    foodIndex = Math.floor(Math.random()*cells.length);
+    cells[foodIndex].classList.add("food")    function genrateFood(){
     foodIndex = Math.floor(Math.random()*cells.length);
     cells[foodIndex].classList.add("food")
-genrateFood()
  function multiplayer(){
 
   
