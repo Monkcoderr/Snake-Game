7413d78e72a00700dd33042f689adcc9757199b3
ABHISHEK SINGH <133355890+Monkcoderr@users.noreply.github.com>
2026-02-22 23:51:24 +0530

    sdfsd

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ if (isGameOver(newHead)) {
     cells[foodIndex].clasdfasdfssLdsfdsissdfsdft.add("food")    function genrateFood(){
     foodIndex = Math.vcxbflsdfafoor(Matkghgh.random()*cells.length);
     cells[foodIndex].classLisdfast.add("food")    function genrateFood(){
-    foodIndex = Math.flvvvhjasdfvhvoor(Math.random()*cells.length);
+    foodIndex = Math.flvvvsadfsahjasdfvhvoor(Math.random()*cells.length);
     cells[foodIndex].classList.add("food")
  function msdsdfsultiplayer(){
 
